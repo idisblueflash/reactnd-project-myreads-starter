ebfd70e60eeb3d6b2d0e6510e76bea9f3e9db4a6
Hu Songtao <idisblueflash@gmail.com>
2017-08-06 14:36:29 +0800

    Fix: backgroundImage can not display bug

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-// TODO: Solve backgroundImage can not display bug.
-const Book = (props) => (
-  <div className="book">
-    <div className="book-top">
-      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("{props.cover}")' }}></div>
-      <div className="book-shelf-changer">
-        <select>
-          <option value="none" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
+class Book extends React.Component{
+  
+  render() {
+    
+    const cover_str = 'url(' + this.props.cover + ')'
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: cover_str }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.authors}</div>
       </div>
-    </div>
-    <div className="book-title">{props.title}</div>
-    <div className="book-authors">{props.authors}</div>
-  </div>
-)
+    )
+  }
+}
 
 class BooksApp extends React.Component {
   state = {
@@ -74,22 +81,9 @@ class BooksApp extends React.Component {
                           authors="Harper Lee" />
                       </li>
                       <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
+                        <Book conver="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+                          title="Ender's Game"
+                          authors="Orson Scott Card" />
                       </li>
                     </ol>
                   </div>
