ab4b801b8128742891046c5c2c9632563243d7b5
Hu Songtao <idisblueflash@gmail.com>
2017-08-06 15:32:51 +0800

    Feat: add new state for want to read books and read book

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ class Bookshelf extends React.Component{
         <div className="bookshelf-books">
           <ol className="books-grid">
             {this.props.books.map((book) => (
-              <li>
-                <Book cover={book.cover}
+              <li key={book.id}>
+                <Book cover={book.imageLinks.thumbnail}
                   title={book.title}
                   authors={book.authors} />
               </li>
@@ -61,6 +61,8 @@ class BooksApp extends React.Component {
     showSearchPage: false,
     books:[],
     current_reading_books:[],
+    want_to_read_books:[],
+    read_books:[],
   }
 
   componentDidMount() {
@@ -103,45 +105,10 @@ class BooksApp extends React.Component {
               <div>
                 <Bookshelf title="Currently Reading" 
                   books={this.state.current_reading_books} />
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book cover="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-                          title="1776"
-                          authors="David McCullough" />
-                      </li>
-                      <li>
-                        <Book cover="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-                          title="Harry Potter and the Sorcerer's Stone"
-                          authors="J.K. Rowling" />
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book cover="http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-                          title="The Hobbit"
-                          authors="J.R.R. Tolkien" />
-                      </li>
-                      <li>
-                        <Book cover="http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-                          title="Oh, the Places You'll Go!"
-                          authors="Seuss" />
-                      </li>
-                      <li>
-                        <Book cover="http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
-                          title="The Adventures of Tom Sawyer"
-                          authors="Mark Twain" />
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+                <Bookshelf title="Want to Read"
+                  books={this.state.want_to_read_books} />
+                <Bookshelf title="Read"
+                  books={this.state.read_books} />
               </div>
             </div>
             <div className="open-search">
