6e98678e0ba8aefbe0986bc886a083494d845aa0
Hu Songtao <idisblueflash@gmail.com>
2017-08-06 14:29:45 +0800

    feat: add Book component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+// TODO: Solve backgroundImage can not display bug.
+const Book = (props) => (
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("{props.cover}")' }}></div>
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{props.title}</div>
+    <div className="book-authors">{props.authors}</div>
+  </div>
+)
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -49,22 +69,9 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
+                        <Book cover="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+                          title="To Kill a Mockingbird"
+                          authors="Harper Lee" />
                       </li>
                       <li>
                         <div className="book">
