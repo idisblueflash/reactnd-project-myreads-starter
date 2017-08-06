a7f8421a44779ec01cb79cb45e5c322969960f71
Hu Songtao <idisblueflash@gmail.com>
2017-08-06 14:38:21 +0800

    Fix: one of book have no cover

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class BooksApp extends React.Component {
                           authors="Harper Lee" />
                       </li>
                       <li>
-                        <Book conver="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+                        <Book cover="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
                           title="Ender's Game"
                           authors="Orson Scott Card" />
                       </li>
