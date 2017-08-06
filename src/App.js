512bc04013f37075d8bc03bf704524eb43b45310
Hu Songtao <idisblueflash@gmail.com>
2017-08-06 15:22:58 +0800

    Feat: simulate current_reading_books for testing

--- src/App.js
+++ src/App.js
@@ -59,29 +59,18 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    books:[]
+    books:[],
+    current_reading_books:[],
   }
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books})
+      this.setState({ current_reading_books: books.filter(book => book.pageCount > 400)})
     })
   }
 
   render() {
-
-    const books_data = [
-      { "cover":"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        "title":"To Kill a Mockingbird",
-        "authors":"Harper Lee"
-      },
-      {
-        "cover":"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        "title":"Ender's Game",
-        "authors":"Orson Scott Card"
-      }
-    ]
-
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -113,7 +102,7 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
                 <Bookshelf title="Currently Reading" 
-                  books={books_data} />
+                  books={this.state.current_reading_books} />
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
