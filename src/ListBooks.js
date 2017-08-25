727ec82fbbc83b700c3bd77f7107c067f0aba735
Hu Songtao <idisblueflash@gmail.com>
2017-08-26 07:14:57 +0800

    refactor: change ListBooks to a stateless pure functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,36 +3,32 @@ import { Link } from 'react-router-dom'
 
 import BookShelf from './BookShelf'
 
-class ListBooks extends React.Component {
-    render() {
-        return (
-             <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <BookShelf title="Currently Reading"
-                            shelf='currentlyReading' 
-                            books={this.props.books}
-                            onMoveBookTo={this.props.onMoveBookTo} />
-                        <BookShelf title="Want to Read"
-                            shelf='wantToRead'
-                            books={this.props.books}
-                            onMoveBookTo={this.props.onMoveBookTo} />
-                        <BookShelf title="Read"
-                            shelf='read'
-                            books={this.props.books}
-                            onMoveBookTo={this.props.onMoveBookTo} />
-                    </div>
-                </div>
-                <div className="open-search">
-                <Link
-                    to="/search" >Add a book</Link>
-                </div>
+const ListBooks = ({ books, onMoveBookTo }) => (
+    <div className="list-books">
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+            <div>
+                <BookShelf title="Currently Reading"
+                    shelf='currentlyReading' 
+                    books={books}
+                    onMoveBookTo={onMoveBookTo} />
+                <BookShelf title="Want to Read"
+                    shelf='wantToRead'
+                    books={books}
+                    onMoveBookTo={onMoveBookTo} />
+                <BookShelf title="Read"
+                    shelf='read'
+                    books={books}
+                    onMoveBookTo={onMoveBookTo} />
             </div>
-        )
-    }
-}
+        </div>
+        <div className="open-search">
+        <Link
+            to="/search" >Add a book</Link>
+        </div>
+    </div>
+);
 
 export default ListBooks
