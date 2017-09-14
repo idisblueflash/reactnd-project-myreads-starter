654b2526739ac65988a76d345225f34127aa1c9e
Hu Songtao <idisblueflash@gmail.com>
2017-09-14 12:03:24 +0800

    refactor: clean duplicated codes

--- src/Book.js
+++ src/Book.js
@@ -3,31 +3,17 @@ import PropTypes from 'prop-types'
 
 class Book extends React.Component{
     render() {
-        let book_cover
-        if (typeof this.props.data.imageLinks === 'undefined'){
-            book_cover = (
-                <div className="book-cover book-default-thumbnail"
-                    style={{ 
-                        width: 128, 
-                        height: 193 }}>
-                </div>
-            )            
-        } else {
-            const thumbnail = this.props.data.imageLinks.thumbnail
-            book_cover = (
-                <div className="book-cover"
-                    style={{ 
-                        width: 128, 
-                        height: 193,
-                        backgroundImage: 'url(' + thumbnail + ')' }}>
-                </div>
-            )
+        let style = {width: 128, height: 193}
+        if (typeof this.props.data.imageLinks !== 'undefined'){
+            style.backgroundImage = 'url(' + this.props.data.imageLinks.thumbnail + ')'
         }
 
         return (
             <div className="book">
                 <div className="book-top">
-                    { book_cover }
+                     <div className="book-cover book-default-thumbnail"
+                        style={style}>
+                    </div>
                     <div className="book-shelf-changer">
                         <select value={this.props.data.shelf || "none"} onChange={(e) => this.props.onMoveBookTo(e, this.props.data)}>
                             <option value="moveto" disabled>Move to...</option>
