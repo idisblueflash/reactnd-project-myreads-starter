aa2623690b0e0e73593740b8a46e69a8b41c83c4
Hu Songtao <idisblueflash@gmail.com>
2017-09-14 11:46:49 +0800

    fix: When a book doesn't have a thumbnail and is missing the imageLinks property, then there's a TypeError

--- src/Book.js
+++ src/Book.js
@@ -3,15 +3,31 @@ import PropTypes from 'prop-types'
 
 class Book extends React.Component{
     render() {
+        let book_cover
+        if (typeof this.props.data.imageLinks === 'undefined'){
+            book_cover = (
+                <div className="book-cover book-default-thumbnail"
+                    style={{ 
+                        width: 128, 
+                        height: 193 }}>
+                </div>
+            )            
+        } else {
+            const thumbnail = this.props.data.imageLinks.thumbnail
+            book_cover = (
+                <div className="book-cover"
+                    style={{ 
+                        width: 128, 
+                        height: 193,
+                        backgroundImage: 'url(' + thumbnail + ')' }}>
+                </div>
+            )
+        }
+
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" 
-                        style={{ 
-                            width: 128, 
-                            height: 193, 
-                            backgroundImage: 'url(' + this.props.data.imageLinks.thumbnail + ')' 
-                        }}></div>
+                    { book_cover }
                     <div className="book-shelf-changer">
                         <select value={this.props.data.shelf || "none"} onChange={(e) => this.props.onMoveBookTo(e, this.props.data)}>
                             <option value="moveto" disabled>Move to...</option>
