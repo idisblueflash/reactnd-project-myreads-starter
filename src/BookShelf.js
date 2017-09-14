14280e5d7576afe6a78570de6038488cb172bc51
Hu Songtao <idisblueflash@gmail.com>
2017-09-14 12:43:20 +0800

    Refactor: convert into a stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,26 +3,20 @@ import PropTypes from 'prop-types'
 
 import Book from './Book'
 
-class BookShelf extends React.Component{ 
-    render() {
-        const books = this.props.books.filter(book => book.shelf === this.props.shelf)
-
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books.map((book) => (
-                            <li key={book.id}>
-                                <Book data={book} onMoveBookTo={this.props.onMoveBookTo}/>
-                            </li>
-                        ))}
-                    </ol>
-                </div>
-            </div>
-        )
-    }
-}
+const BookShelf = ({ title, shelf, books, onMoveBookTo}) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                {books.filter(book => book.shelf === shelf).map((book) => (
+                    <li key={book.id}>
+                        <Book data={book} onMoveBookTo={onMoveBookTo}/>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    </div>
+)
 
 BookShelf.propTypes = {
     onMoveBookTo: PropTypes.func.isRequired,
